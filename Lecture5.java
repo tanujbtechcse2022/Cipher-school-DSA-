22e93c8b9c15d4b30fab218962452452c8df7a09
Tanuj <116993813+tanujbtechcse2022@users.noreply.github.com>
2024-06-08 02:24:12 +0530

    Add files via upload

---
+++ Lecture5.java
@@ -0,0 +1,27 @@
+class Lecture5 {
+    public static void main(String args[]){
+        float f= (float)5.5; //Consider decimal to double
+        float f2=5.5f;
+        double d=10.5;
+        char ch='a';
+        System.out.println("F is "+f);
+        System.out.println("f2 is "+f2);
+        System.out.println("D is "+d);
+        System.out.println(ch);
+        
+        //int z=x+y;
+        //+ , -  are operator
+        //x,y are operands
+        
+        
+        //Increment and decrement operator
+        
+        int m=5;
+        int n=m++;
+        System.out.println("M is : "+m+", N is : "+n);
+        
+        int p=5;
+        int q=++p;
+        System.out.println("P is : "+p+", Q is : "+q);
+    }
+}
